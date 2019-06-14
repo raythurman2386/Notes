dab3be58423a191c62f621d90fe3652a21ea0ff9
raythurman2386 <raymondthurman5@gmail.com>
2019-06-13 22:36:16 -0400

    added CM files and images

---
+++ LambdaNotes/Section Lead/CMharassment2.pl
@@ -0,0 +1,3 @@
+Lol. Don’t be weirded out.  You just caught my eye and I wanted to say.... I think I’m getting this whole idea of responsive design now
+
+Promise this will be the last message, if you’re not amused
---
+++ LambdaNotes/Section Lead/montoyaHarassment.pl
@@ -0,0 +1,6 @@
+
+Charlie Montoya 8:59 PM
+Yep... totally DMing out of the blue... cause why not... between you and Kieran ... honestly... you’re easier on the eyes... and you just yawned... so why not some excitement
+No what? No DM :rolling_on_the_floor_laughing:
+No yawn
+I’m not a great lip reader
