da8c3e7656c45cf36952d03876fd7ffb92171241
raythurman2386 <raymondthurman5@gmail.com>
2019-12-25 20:32:18 -0500

    native todo portion

--- Frontend Masters/react-native/react-web-starter-master/src/app/Todo.js
+++ Frontend Masters/react-native/react-web-starter-master/src/app/Todo.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
 export class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      newTodo: ''
+    }
+  }
+
+  handleChange(e) {
+    const { value } = e.target;
+    this.setState({ newTodo: value })
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    const todos = [...this.state.todos, this.state.newTodo]
+    this.setState({ todos, newTodo: '' })
+  }
+
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <form onSubmit={e => this.handleClick(e)}>
+          <input value={this.state.newtodo} onChange={(e) => this.handleChange(e)} type="text" placeholder="New Todo" />
+          <button>Click</button>
+        </form>
+        <ul>
+          {this.state.todos.map(todo => <li>{todo}</li>)}
+        </ul>
+      </div>
     );
   }
 }
