fdaa57ac8a05f5ea02f91eee2c6c358ee2add4bb
Ray Thurman <raymondthurman5@gmail.com>
2020-03-12 10:45:45 -0400

    end of file system notes

--- LinkedInLearning/NodeEssential/list.js
+++ LinkedInLearning/NodeEssential/list.js
@@ -27,3 +27,55 @@ fs.readFile('./assets/alex.jpg', 'UTF-8', (err, img) => {
 
   console.log(img)
 })
+
+// Writefile
+const md = `
+# This is a new file
+
+We can write text to a file with fs.writefile
+
+* fs.readdir
+* fs.readFile
+* fs.writeFile
+`
+
+fs.writeFile('./assets/note.md', md.trim(), (err) => {
+  if(err) {
+    throw err
+  }
+
+  console.log('File saved')
+})
+
+fs.mkdir("storage-files", err => {
+  if(err) {
+    throw err
+  }
+
+  console.log("Directory Created")
+})
+
+// Append Files
+const colorData = require('./assets/colors.json')
+
+colorData.colorList.forEach(
+  color => {
+    fs.appendFile('./storage-files/colors.md', `${color.color}: ${color.hex} \n`, 
+    err => {
+      if(err) {
+        throw err
+      }
+    })
+})
+
+// Rename
+fs.renameSync("./assets/colors.json", './assets/colorData.json')
+fs.rename("./assets/notes.md", "./storage-files/notes.md", err => {
+  if(err) {
+    throw err
+  }
+})
+
+setTimeout(() => {
+  fs.unlinkSync("./assets/alex.jpg")
+}, 4000)
