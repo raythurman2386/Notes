a3529823f9713e319b9e41c4dac3406d9a587806
Raymond Thurman <raymondthurman5@gmail.com>
2020-05-20 22:25:46 -0400

    grade students algorithm completed

--- algorithms/grade_students.py
+++ algorithms/grade_students.py
@@ -1,8 +1,13 @@
 def grade_students(grades):
-    pass
+
+    for grade in grades:
+        if grade < 38 or grade % 5 < 3:
+            return grade
+        else:
+            return grade + (5 - (grade % 5))
 
 
-grade_students([73, 67, 38, 33])
+print(grade_students([73, 67, 38, 33]))
 
 # CamelCase
 print(sum(map(str.isupper, input())) + 1)
