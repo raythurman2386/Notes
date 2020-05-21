5b67a6c4af98c0e8ff9c76ec696b7882adbc4cec
Raymond Thurman <raymondthurman5@gmail.com>
2020-05-20 22:34:44 -0400

    fixed grade students algorithm

--- algorithms/grade_students.py
+++ algorithms/grade_students.py
@@ -1,10 +1,13 @@
 def grade_students(grades):
+    updated_grades = []
 
     for grade in grades:
         if grade < 38 or grade % 5 < 3:
-            return grade
+            updated_grades.append(grade)
         else:
-            return grade + (5 - (grade % 5))
+            updated_grades.append(grade + (5 - (grade % 5)))
+
+    return updated_grades
 
 
 print(grade_students([73, 67, 38, 33]))
