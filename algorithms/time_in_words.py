a64a37ae83fcefd964b69fc499ca396a054e21ba
Ray Thurman <raymondthurman5@gmail.com>
2020-05-22 10:32:20 -0400

    initial refactor of time_in_words

--- algorithms/time_in_words.py
+++ algorithms/time_in_words.py
@@ -31,64 +31,63 @@ def timeInWords(h, m):
       "13": "thirteen",
       "14": "forteen",
       "15": "quarter",
-      "16": "six-teen",
-      "17": "seven-teen",
-      "18": "eight-teen",
-      "19": "nine-teen",
+      "16": "six teen",
+      "17": "seven teen",
+      "18": "eight teen",
+      "19": "nine teen",
       "20": "twenty",
-      "21": "twenty-one",
-      "22": "twenty-two",
-      "23": "twenty-three",
-      "24": "twenty-four",
-      "25": "twenty-five",
-      "26": "twenty-six",
-      "27": "twenty-seven",
-      "28": "twenty-eight",
-      "29": "twenty-nine",
+      "21": "twenty one",
+      "22": "twenty two",
+      "23": "twenty three",
+      "24": "twenty four",
+      "25": "twenty five",
+      "26": "twenty six",
+      "27": "twenty seven",
+      "28": "twenty eight",
+      "29": "twenty nine",
       "30": "half"
     }
 
     min_string = "o' clock"
-    hour_string = ""
+    hour_string = hour_arr[h - 1]
+    initial_str = str(m)
+    remainder = str(60 - m)
     result = ""
     # Set the Hours to a string
     # If greater than 30
     # Hour is equal to the following hour
-
-    if m > 30:
-        hour_string = hour_arr[h]
-    else:
-        hour_string = hour_arr[h - 1]
-
     # Convert the minutes to a string
     # if 15 min past the hour or before the new hour,
     # min_string will equal quarter past
-    if m == 1:
-        initial_str = str(m)
-        if initial_str in min_dict.keys():
+    if initial_str in min_dict.keys():
+        if m == 1:
             min_string = min_dict[initial_str]
             result = f"{min_string} minute past {hour_string}"
 
-    elif m in range(2, 30):
-        initial_str = str(m)
-        if initial_str in min_dict.keys():
+        elif m in range(2, 30):
             min_string = min_dict[initial_str]
             result = f"{min_string} minutes past {hour_string}"
 
             if min_string == "quarter":
                 result = f"{min_string} past {hour_string}"
 
-    elif m > 30:
-        remainder = str(60 - m)
-        if remainder in min_dict.keys():
+        elif m == 30:
+            min_string = min_dict[initial_str]
+            result = f"{min_string} past {hour_string}"
+
+    elif remainder in min_dict.keys():
+        if m > 30:
+            hour_string = hour_arr[h]
             min_string = min_dict[remainder]
             result = f"{min_string} minutes to {hour_string}"
 
             if min_string == "quarter":
                 result = f"{min_string} to {hour_string}"
 
+    else:
+        result = f"{hour_string} {min_string}"
 
     return result
 
 
-timeInWords(5, 28)
+print(timeInWords(5, 0))
