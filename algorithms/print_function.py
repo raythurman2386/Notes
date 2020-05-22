726373518ac15cecd890a2a6c120d16a94f05033
Ray Thurman <raymondthurman5@gmail.com>
2020-05-22 09:39:05 -0400

    initial time in words algo implementation

--- algorithms/print_function.py
+++ algorithms/print_function.py
@@ -1,17 +1,39 @@
 if __name__ == '__main__':
-    n = int(input('Please Supply a number: '))
+    answers = {
+        "num": 0,
+        "start": 0,
+        "count": 0
+    }
 
-    i = 1
+    questions = [
+        'Please Supply a number: ', 
+        'Please Pick a Starting Point: ', 
+        'What would you like to count by? '
+    ]
+
+    for key in answers.keys():
+        index = 0
+        if index < len(questions):
+
+            question = questions[index]
+            answers[key] = int(input(questions[index]))
+            index += 1
+
+    # n = int(input('Please Supply a number: '))
+    # j = int(input('Please Pick a Starting Point: '))
+    # count = int(input('What would you like to count by? '))
+
+    i = answers["start"]
     arr = []
     strArr = []
     results = ""
 
-    while i <= n:
+    while i <= answers["num"]:
         arr.append(i)
-        i += 1
+        i += answers["count"]
 
     for num in arr:
        j = str(num)
        strArr.append(j)
 
-    print("".join(strArr))
+    print("-".join(strArr))
---
+++ algorithms/time_in_words.py
@@ -0,0 +1,94 @@
+# Complete the timeInWords function below.
+def timeInWords(h, m):
+    hour_arr = [
+      "one", 
+      "two", 
+      "three", 
+      "four", 
+      "five", 
+      "six", 
+      "seven", 
+      "eight", 
+      "nine", 
+      "ten", 
+      "eleven", 
+      "twelve"
+    ]
+
+    min_dict = {
+      "1": "one",
+      "2": "two",
+      "3": "three",
+      "4": "four",
+      "5": "five",
+      "6": "six",
+      "7": "seven",
+      "8": "eight",
+      "9": "nine",
+      "10": "ten",
+      "11": "eleven",
+      "12": "twelve",
+      "13": "thirteen",
+      "14": "forteen",
+      "15": "quarter",
+      "16": "six-teen",
+      "17": "seven-teen",
+      "18": "eight-teen",
+      "19": "nine-teen",
+      "20": "twenty",
+      "21": "twenty-one",
+      "22": "twenty-two",
+      "23": "twenty-three",
+      "24": "twenty-four",
+      "25": "twenty-five",
+      "26": "twenty-six",
+      "27": "twenty-seven",
+      "28": "twenty-eight",
+      "29": "twenty-nine",
+      "30": "half"
+    }
+
+    min_string = "o' clock"
+    hour_string = ""
+    result = ""
+    # Set the Hours to a string
+    # If greater than 30
+    # Hour is equal to the following hour
+
+    if m > 30:
+        hour_string = hour_arr[h]
+    else:
+        hour_string = hour_arr[h - 1]
+
+    # Convert the minutes to a string
+    # if 15 min past the hour or before the new hour,
+    # min_string will equal quarter past
+    if m == 1:
+        initial_str = str(m)
+        if initial_str in min_dict.keys():
+            min_string = min_dict[initial_str]
+            result = f"{min_string} minute past {hour_string}"
+
+    elif m in range(2, 30):
+        initial_str = str(m)
+        if initial_str in min_dict.keys():
+            min_string = min_dict[initial_str]
+            result = f"{min_string} minutes past {hour_string}"
+
+            if min_string == "quarter":
+                result = f"{min_string} past {hour_string}"
+
+    elif m > 30:
+        remainder = str(60 - m)
+        if remainder in min_dict.keys():
+            min_string = min_dict[remainder]
+            result = f"{min_string} minutes to {hour_string}"
+
+            if min_string == "quarter":
+                result = f"{min_string} to {hour_string}"
+
+
+    return result
+
+
+timeInWords(5, 28)
