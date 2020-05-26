b91fd07543461df1f809d7b2791f4ec570340cdb
Raymond Thurman <raymondthurman5@gmail.com>
2020-05-26 18:33:09 -0400

    removed arr length arg from insertion sort 2 algo

--- algorithms/insertion_sort_pt2.py
+++ algorithms/insertion_sort_pt2.py
@@ -1,8 +1,9 @@
-def insertionSort2(n, arr):
+def insertionSort2(arr):
     a = arr
-    for i in range(1, n):
+    for i in range(1, len(arr)):
         index = next(iter(j for j in range(i) if a[j] > a[i]), i)
         a[index+1:i+1], a[index] = a[index:i], a[i]
         print(*a)
 
-insertionSort2(6, [1,4,3,5,6,2])
+
+insertionSort2([1, 4, 3, 5, 6, 2])
