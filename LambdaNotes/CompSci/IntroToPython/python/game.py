e19b2e5f24ea94753faa94baf8ab9c6bc6a3449c
Raymond Thurman <raymondthurman5@gmail.com>
2020-06-03 20:23:49 -0400

    minor refactor to rock paper scissors game

--- LambdaNotes/CompSci/IntroToPython/python/game.py
+++ LambdaNotes/CompSci/IntroToPython/python/game.py
@@ -3,28 +3,38 @@ import random
 
 # Add ability to load saved data
 def load_game():
-    text_file = open("history.txt", "r")
-    history = text_file.read().split(',')
-    text_file.close()
+    with open("history.txt", "r") as f:
+        history = f.read().split(',')
     return history
 
 
 # Add ability to save data
 def save_game(w, t, l):
-    text_file = open("history.txt", 'w')
-    text_file.write(f"{w}, {t}, {l}")
-    text_file.close()
+    with open("history.txt", 'w') as f:
+        f.write(f"{w}, {t}, {l}")
 
 
+# Print the score
+def print_score(w, t, l):
+    print(f"Wins: {w}, Ties: {t}, losses: {l}\n")
+    print("Please choose to continue . . .")
+
+
+def user_choice():
+    return int(input("[1] Rock [2] Paper [3] Scissors [9] Quit\n"))
+
+
+def comp_choice():
+    return random.randint(1, 3)
+
+
+# Initialize Game
 results = load_game()
 wins, loss, ties = int(results[0]), int(results[1]), int(results[2])
 print("Welcome to Rock, Paper, Scissors!")
-print(f"Wins: {wins}, Ties: {ties}, losses: {loss}")
-print("Please chose to continue . . .")
-
-# Initialize Choices
-computer = random.randint(1, 3)
-user = int(input("[1] Rock [2] Paper [3] Scissors [9] Quit\n"))
+print_score(wins, ties, loss)
+computer = comp_choice()
+user = user_choice()
 
 while not user == 9:
     # User chooses rock
@@ -65,9 +75,8 @@ while not user == 9:
     else:
         print('Please choose only 1, 2, or 3 for rock, paper, or scissors')
 
-    print(f"Wins: {wins}, Ties: {ties}, losses: {loss}\n")
-    print("Please choose to continue . . .")
-    computer = random.randint(1, 3)
-    user = int(input("[1] Rock [2] Paper [3] Scissors [9] Quit\n"))
+    print_score(wins, ties, loss)
+    computer = comp_choice()
+    user = user_choice()
 
 save_game(wins, ties, loss)
