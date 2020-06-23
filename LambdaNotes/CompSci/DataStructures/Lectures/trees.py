79afe2a0525a81e6f6ba57a9d3b1507fc11653cb
Raymond Thurman <raymondthurman5@gmail.com>
2020-06-22 21:45:47 -0400

    bst lecture minimal notes

---
+++ LambdaNotes/CompSci/DataStructures/Lectures/trees.py
@@ -0,0 +1,6 @@
+"""
+Binary Search Tree is a node-based binary tree data structure which has the following properties:
+    - The left subtree of a node contains only nodes with keys(values) lesser than the parent nodes key
+    - The right subtree of a node contains only nodes with keys(values) greater than the parent nodes key 
+    - The left and right subtree each must also be a binary search tree.
+"""
