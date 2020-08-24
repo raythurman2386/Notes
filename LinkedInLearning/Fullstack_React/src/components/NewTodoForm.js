fa3f30956a35c3d22bca5ec2e40bbb8fdcf3a9f4
Raymond Thurman <raymondthurman5@gmail.com>
2020-08-24 11:01:14 -0400

    :tada: redux setup implemented

--- LinkedInLearning/Fullstack_React/src/components/NewTodoForm.js
+++ LinkedInLearning/Fullstack_React/src/components/NewTodoForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { createTodo } from '../actions';
 
-const NewTodoForm = () => {
+const NewTodoForm = ({ todos, onCreatePressed }) => {
 	const [value, setValue] = useState('');
 
 	return (
@@ -11,9 +13,27 @@ const NewTodoForm = () => {
 				placeholder='New Todo'
 				onChange={(e) => setValue(e.target.value)}
 			/>
-			<button>Create Todo</button>
+			<button
+				onClick={() => {
+					const isDuplicate = todos.some((todo) => todo.text === value);
+					if (!isDuplicate) {
+						onCreatePressed(value);
+						setValue('');
+					}
+				}}
+			>
+				Create Todo
+			</button>
 		</div>
 	);
 };
 
-export default NewTodoForm;
+const mapStateToProps = (state) => ({
+	todos: state.todos,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	onCreatePressed: (text) => dispatch(createTodo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
--- LinkedInLearning/Fullstack_React/src/components/TodoListItem.js
+++ LinkedInLearning/Fullstack_React/src/components/TodoListItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const TodoListItem = ({ todo }) => {
+const TodoListItem = ({ todo, onRemovePressed }) => {
 	return (
 		<div className='todo-item-container'>
 			<h3>{todo.text}</h3>
 			<div className='button-container'>
 				<button className='button-complete'>Complete</button>
-				<button className='button-remove'>Remove</button>
+				<button
+					onClick={() => onRemovePressed(todo.text)}
+					className='button-remove'
+				>
+					Remove
+				</button>
 			</div>
 		</div>
 	);
