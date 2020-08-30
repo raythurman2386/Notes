74b313c2e2dbceaec67fd7793aae24f4f41b923b
Raymond Thurman <raymondthurman5@gmail.com>
2020-08-30 19:38:53 -0400

    added codewars pick peaks algorithm

--- algorithms/codewars/pickPeaks.js
+++ algorithms/codewars/pickPeaks.js
@@ -6,15 +6,19 @@ In this kata, you will write a function that returns the positions and the value
 
 For example, the array arr = [0, 1, 2, 5, 1, 0] has a peak at position 3 with a value of 5 (since arr[3] equals 5).
 
-The output will be returned as an object with two properties: pos and peaks. Both of these properties should be arrays. If there is no peak in the given array, then the output should be {pos: [], peaks: []}.
+The output will be returned as an object with two properties: pos and peaks. Both of these properties should be arrays. 
+If there is no peak in the given array, then the output should be {pos: [], peaks: []}.
 
 Example: pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]) should return {pos: [3, 7], peaks: [6, 3]} (or equivalent in other languages)
 
 All input arrays will be valid integer arrays (although it could still be empty), so you won't need to validate the input.
 
-The first and last elements of the array will not be considered as peaks (in the context of a mathematical function, we don't know what is after and before and therefore, we don't know if it is a peak or not).
+The first and last elements of the array will not be considered as peaks (in the context of a mathematical function, we don't know what is 
+after and before and therefore, we don't know if it is a peak or not).
 
-Also, beware of plateaus !!! [1, 2, 2, 2, 1] has a peak while [1, 2, 2, 2, 3] does not. In case of a plateau-peak, please only return the position and value of the beginning of the plateau. For example: pickPeaks([1, 2, 2, 2, 1]) returns {pos: [1], peaks: [2]} (or equivalent in other languages)
+Also, beware of plateaus !!! [1, 2, 2, 2, 1] has a peak while [1, 2, 2, 2, 3] does not. In case of a plateau-peak, please only return the 
+position and value of the beginning of the plateau. For example: pickPeaks([1, 2, 2, 2, 1]) returns {pos: [1], peaks: [2]} (or equivalent 
+in other languages)
 
 */
 
@@ -24,5 +28,114 @@ function pickPeaks(arr) {
 		peaks: [],
 	};
 
+	// Find index in array that is higher than n - 1 and n + 1
+	// add that index to result.pos and the number to result.peaks
+	for (let i = 0; i < arr.length; i++) {
+		// console.log(arr[i])
+		if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+			// if the current index is greater than the previous
+			// and greater than the next index
+			// add index and number to object
+			result.pos.push(i);
+			result.peaks.push(arr[i]);
+		}
+		// Passes all basic tests without this section
+		// else if (arr[i] > arr[i - 1] && arr[i] == arr[i + 1]) {
+		// 	result.pos.push(i);
+		// 	result.peaks.push(arr[i]);
+		// }
+		else if (
+			// else if there is a plateau
+			// add the first index where plateau starts
+			arr[i] > arr[i - 1] &&
+			arr[i] === arr[i + 1] &&
+			arr[i] > arr[arr.length - 1]
+		) {
+			result.pos.push(i);
+			result.peaks.push(arr[i]);
+		}
+	}
+
 	return result;
 }
+
+console.log(pickPeaks([0, 1, 2, 5, 1, 0]));
+console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]));
+console.log(pickPeaks([1, 2, 2, 2, 1]));
+console.log(pickPeaks([1, 2, 2, 2, 3]));
+// Passing with current implementation
+console.log(
+	pickPeaks([
+		2,
+		-2,
+		10,
+		13,
+		13,
+		-3,
+		6,
+		14,
+		4,
+		2,
+		10,
+		11,
+		0,
+		7,
+		-3,
+		-2,
+		7,
+		3,
+		12,
+		15,
+		0,
+		1,
+		-4,
+		13,
+		2,
+		-2,
+		10,
+		-3,
+		3,
+		0,
+		-4,
+		6,
+	])
+);
+
+// Failing Test
+// Needs to equal === { pos: [ 2, 4, 7, 9, 11, 13, 16, 18, 22, 24, 28 ],
+// 											peaks: [11, 11, 11, 14, 14, 15, 15, 15, 12, 11, 9] }
+console.log(
+	pickPeaks([
+		12,
+		0,
+		11,
+		-1,
+		11,
+		11,
+		-1,
+		11,
+		0,
+		14,
+		11,
+		14,
+		3,
+		15,
+		2,
+		14,
+		15,
+		11,
+		15,
+		-3,
+		1,
+		6,
+		12,
+		6,
+		11,
+		7,
+		7,
+		-1,
+		9,
+		3,
+		11,
+	])
+);
--- scratch.js
+++ scratch.js
@@ -1,67 +1,70 @@
-// TODO: for (var i = 0; i < 3; i++) {
-//   setTimeout(function () { alert(i); }, 1000 + i);
+// // TODO: for (var i = 0; i < 3; i++) {
+// //   setTimeout(function () { alert(i); }, 1000 + i);
+// // }
+
+// (function () {
+//   var a = (b = 5);
+// })();
+
+// console.log(b);
+
+// // CamelCase
+// function camalize(str) {
+//   return str
+//     .toLowerCase()
+//     .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
 // }
 
-(function () {
-  var a = (b = 5);
-})();
+// console.log(camalize("How *are -You _doing"));
 
-console.log(b);
+// // state machine in JS
+// const machine = {
+//   dispatch(actionName, ...payload) {
+//     const actions = this.transitions[this.state];
+//     const action = this.transitions[this.state][actionName];
 
-// CamelCase
-function camalize(str) {
-  return str
-    .toLowerCase()
-    .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
-}
+//     if (action) {
+//       action.apply(machine, ...payload);
+//     }
+//   },
+//   changeStateTo(newState) {
+//     this.state = newState;
+//   },
+//   state: "idle",
+//   transitions: {
+//     idle: {
+//       click: () => {
+//         this.changeStateTo("fetching");
+//         service.getData().then(
+//           data => {
+//             try {
+//               this.dispatch("success", JSON.parse(data));
+//             } catch (error) {
+//               this.dispatch("failure", error);
+//             }
+//           },
+//           error => this.dispatch("failure", error)
+//         );
+//       }
+//     },
+//     fetching: {
+//       success: data => {
+//         this.changeStateTo("idle");
+//       },
+//       failure: err => {
+//         this.changeStateTo("error");
+//       }
+//     },
+//     error: {
+//       retry: () => {
+//         this.changeStateTo("idle");
+//         this.dispatch("click");
+//       }
+//     }
+//   }
+// };
 
-console.log(camalize("How *are -You _doing"));
+// machine.dispatch("click");
 
-// state machine in JS
-const machine = {
-  dispatch(actionName, ...payload) {
-    const actions = this.transitions[this.state];
-    const action = this.transitions[this.state][actionName];
 
-    if (action) {
-      action.apply(machine, ...payload);
-    }
-  },
-  changeStateTo(newState) {
-    this.state = newState;
-  },
-  state: "idle",
-  transitions: {
-    idle: {
-      click: () => {
-        this.changeStateTo("fetching");
-        service.getData().then(
-          data => {
-            try {
-              this.dispatch("success", JSON.parse(data));
-            } catch (error) {
-              this.dispatch("failure", error);
-            }
-          },
-          error => this.dispatch("failure", error)
-        );
-      }
-    },
-    fetching: {
-      success: data => {
-        this.changeStateTo("idle");
-      },
-      failure: err => {
-        this.changeStateTo("error");
-      }
-    },
-    error: {
-      retry: () => {
-        this.changeStateTo("idle");
-        this.dispatch("click");
-      }
-    }
-  }
-};
-
-machine.dispatch("click");
+const testing = fetch('https://www.erome.com/a/HoCVoaxj').then(res => res.json()).then(data => console.log(data))
