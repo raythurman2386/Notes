264d558f36b172fb88e5210f4f9915deb6a7741a
Raymond Thurman <raymondthurman5@gmail.com>
2020-10-01 08:31:34 -0400

    ✨ backend subscriptions hooked up

--- Traversy/GraphQL_Chat/server/server.js
+++ Traversy/GraphQL_Chat/server/server.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer, PubSub } = require('graphql-yoga');
 
 const messages = [];
 
@@ -16,7 +16,15 @@ const typeDefs = `
   type Mutation {
     postMessage(user: String!, content: String!): ID!
   }
+
+  type Subscription {
+    messages: [Message!]
+  }
 `;
+
+// keep track of the users
+const subscribers = [];
+const onMessagesUpdate = (fn) => subscribers.push(fn);
 
 const resolvers = {
 	Query: {
@@ -30,12 +38,26 @@ const resolvers = {
 				user,
 				content,
 			});
+			// Alert the system when new message arrives
+			subscribers.forEach((fn) => fn());
 			return id;
+		},
+	},
+	Subscription: {
+		messages: {
+			subscribe: (parent, args, { pubsub }) => {
+				const channel = Math.random().toString(36).slice(2, 15);
+				onMessagesUpdate(() => pubsub.publish(channel, { messages }));
+				setTimeout(() => pubsub.publish(channel, { messages }), 0);
+				return pubsub.asyncIterator(channel);
+			},
 		},
 	},
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+// PubSub for subscriptions
+const pubsub = new PubSub();
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 server.start(({ port }) => {
 	console.log(`Server started on http://localhost:${port}`);
 });
