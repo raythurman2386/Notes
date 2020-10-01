05c3a107e7ce14654251ab967d36f6ddd5a7d47c
Raymond Thurman <raymondthurman5@gmail.com>
2020-10-01 08:33:38 -0400

    ✨ frontend working with subs

--- Traversy/GraphQL_Chat/client/src/Chat.jsx
+++ Traversy/GraphQL_Chat/client/src/Chat.jsx
@@ -3,19 +3,30 @@ import {
 	ApolloClient,
 	InMemoryCache,
 	ApolloProvider,
+	useSubscription,
 	useQuery,
 	useMutation,
 	gql,
 } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
 import { Container, Row, Col, FormInput, Button } from 'shards-react';
 
+// Link for our subscription to messages
+const link = new WebSocketLink({
+	uri: `ws://localhost:4000/`,
+	options: {
+		reconnect: true,
+	},
+});
+
 const client = new ApolloClient({
+	link,
 	uri: 'http://localhost:4000/',
 	cache: new InMemoryCache(),
 });
 
 const GET_MESSAGES = gql`
-	query {
+	subscription {
 		messages {
 			id
 			user
@@ -31,7 +42,7 @@ const POST_MESSAGE = gql`
 `;
 
 const Messages = ({ user }) => {
-	const { data } = useQuery(GET_MESSAGES, { pollInterval: 500 });
+	const { data } = useSubscription(GET_MESSAGES);
 	if (!data) {
 		return null;
 	}
